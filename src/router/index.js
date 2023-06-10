ee99cd7f00435b993af0d26e54b72258b377f9ff
seal <yejunhai2001@gmail.com>
2023-06-10 20:30:29 +0800

    add @ path

--- src/router/index.js
+++ src/router/index.js
@@ -1,59 +1,59 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Layout from "../components/Layout.vue";
+import Layout from "@/components/Layout.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "dashboard",
     component: Layout,
     redirect: "/users",
     children: [
       {
         path: "users",
         name: "users",
-        component: () => import("../pages/users/index.vue"),
+        component: () => import("@/pages/users/index.vue"),
       },
       {
         path: "categories",
         name: "categories",
-        component: () => import("../pages/categories/index.vue"),
+        component: () => import("@/pages/categories/index.vue"),
       },
       {
         path: "goods",
         name: "goods",
-        component: () => import("../pages/goods/index.vue"),
+        component: () => import("@/pages/goods/index.vue"),
       },
       {
         path: "orders",
         name: "orders",
-        component: () => import("../pages/orders/index.vue"),
+        component: () => import("@/pages/orders/index.vue"),
       },
       {
         path: "params",
         name: "params",
-        component: () => import("../pages/params/index.vue"),
+        component: () => import("@/pages/params/index.vue"),
       },
       {
         path: "reports",
         name: "reports",
-        component: () => import("../pages/reports/index.vue"),
+        component: () => import("@/pages/reports/index.vue"),
       },
       {
         path: "rights",
         name: "rights",
-        component: () => import("../pages/rights/index.vue"),
+        component: () => import("@/pages/rights/index.vue"),
       },
       {
         path: "roles",
         name: "roles",
-        component: () => import("../pages/roles/index.vue"),
+        component: () => import("@/pages/roles/index.vue"),
       },
     ],
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("../pages/login/index.vue"),
+    component: () => import("@/pages/login/index.vue"),
   },
 ];
 
