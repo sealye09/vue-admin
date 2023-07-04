65aa5802a4d66f89bc5ba73127546d6b271cb62f
seal <yejunhai2001@gmail.com>
2023-07-04 16:34:33 +0800

    #

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -7,27 +7,26 @@ const num = ref(0);
 </script>
 
 <template>
-  <div>
-    <span class="text-xl text-amber-600 hover:text-orange-600/80">dashboard</span>
-    <div class="flex flex-col gap-4 my-2">
-      <div class="flex gap-2 items-center">
-        <span>Vue ref</span>
-        <el-button
-          type="primary"
-          @click="num++"
-        >
-          {{ num }}
-        </el-button>
-      </div>
-      <div class="flex gap-4 items-center">
-        <span>pinia </span>
-        <el-button
-          type="primary"
-          @click="countStore.increment"
-        >
-          {{ countStore.num }}
-        </el-button>
-      </div>
+  <span class="text-xl text-amber-600 hover:text-orange-600/80">dashboard</span>
+  <el-divider />
+  <div class="flex flex-col gap-4 my-2">
+    <div class="flex gap-2 items-center">
+      <span>Vue ref</span>
+      <el-button
+        type="primary"
+        @click="num++"
+      >
+        {{ num }}
+      </el-button>
+    </div>
+    <div class="flex gap-4 items-center">
+      <span>pinia </span>
+      <el-button
+        type="primary"
+        @click="countStore.increment"
+      >
+        {{ countStore.num }}
+      </el-button>
     </div>
   </div>
 </template>
